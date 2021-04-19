ad58ef318152d8d5f5a026efcc527bd9c220584d
Dwight Fonseka <32918413+dwightfonseka@users.noreply.github.com>
2021-04-19 19:13:22 +0800

    Add files via upload

--- meta2.R
+++ meta2.R
@@ -43,63 +43,32 @@ meta1$SE = (log(meta1$uppCI) - log(meta1$lowCI))/3.92
 #######################
 
 library(dplyr)
-meta2a = meta1 %>% filter(category=="cat1")
-meta2b = meta1 %>% filter(category=="cat2")
 
-meta2c = meta1 %>% filter(category=="cat1") %>% filter(Class=="duration")
-meta2d= meta1 %>% filter(category=="cat2") %>% filter(Class=="duration")
+meta1a = meta1 %>% filter(category=="cat1") %>%
+  filter(Class=="duration")
 
+meta1a1 = meta1 %>% filter(category=="cat1") %>%
+  filter(Class=="duration") %>% filter(studyType=="Cross-sectional")
 
-
-### new ones
-
-meta2e = meta1 %>% filter(Class=="duration") %>%
-  filter(category=="cat1") %>% filter(studyType=="Cross-sectional")
-meta2f = meta1 %>% filter(Class=="duration") %>%
-  filter(category=="cat1") %>% filter(studyType=="Prospective")
-meta2g = meta1 %>% filter(Class=="duration") %>%
-    filter(studyType=="Cross-sectional")
-meta2h = meta1 %>% filter(Class=="duration") %>%
-   filter(studyType=="Prospective")
-  
-
-meta2i = meta1 %>% 
-  filter(category=="cat1") %>% filter(studyType=="Cross-sectional")
-meta2j = meta1 %>% 
-  filter(category=="cat1") %>% filter(studyType=="Prospective")
-meta2k = meta1 %>%
-  filter(studyType=="Cross-sectional")
-meta2l = meta1 %>%
-  filter(studyType=="Prospective")
+meta1a2 = meta1 %>% filter(category=="cat1") %>%
+  filter(Class=="duration") %>% filter(studyType=="Prospective")
 
 
 
-####################
+meta2a = meta1 %>% filter(Class=="duration")
 
-meta3a = meta2a   # cat1   YesNo + duration
-meta3b =  meta2c   # cat1   duration only
-meta3c = rbind(meta2a, meta2b)   # cat1 + cat2   YesNo + duration
-meta3d = rbind(meta2c, meta2d)  # cat1 + cat2    duration only
+meta2a1 = meta1 %>% 
+  filter(Class=="duration") %>% filter(studyType=="Cross-sectional")
 
-meta3u = rbind(meta2i,meta2j)
-meta3p = rbind(meta2k,meta2l)
-
-################### selection
-
-# model 1a > all cross sectional cat 1  > meta2i
-# model 1b > all propsective cat 1      > meta2j
-# model 1c > screen cross sectional cat 1 > meta2e
-# model 1d > screen prospectitive cat 1 > meta2f
-# model 2a > all cat 1 & cat 2          > meta 3c
-# model 2b > screen cat 1 & cat2        > meta 3d
-
-meta2 = meta3d        #i,j,e,f, 3c, 3d
-
+meta2a2 = meta1 %>%
+  filter(Class=="duration") %>% filter(studyType=="Prospective")
 
 
 ### create forest plot and summary function
 
-par1 = function(meta2){
+options(OutDec="\xB7")
+
+model = function(meta2){
   
   library(meta)
   
@@ -121,18 +90,14 @@ par1 = function(meta2){
     ss=samp,
     aut=aut
   )
- 
-}  
-  
-forest1 = function(meta2){  
-   
+
   library(meta)
   res2 = metagen(parDF$rr, 
                  parDF$se, 
                  data=parDF,
-                 byvar=parDF$group,
+               #  byvar=parDF$group,
                  studlab=parDF$aut,
-                # n.e=parDF$ss,
+                 n.e=parDF$ss,
                  overall=TRUE,
                  comb.random=TRUE, 
                  comb.fixed=TRUE,
@@ -142,8 +107,6 @@ forest1 = function(meta2){
                  prediction = FALSE,
                  sm = "OR")
   
-  #jpeg("C://Users//d_n_f//Desktop//rplot.jpg", width = 750, height = 750)
-  
   
   forest.meta(res2,
          leftcols = c('studlab','rr','se'),
@@ -152,8 +115,6 @@ forest1 = function(meta2){
            )
   
 
-  #dev.off()
-  
   s1=summary.meta(res2,
                overall.hetstat = TRUE, 
                overall = TRUE)
@@ -165,96 +126,14 @@ forest1 = function(meta2){
 }
 
 
-### bias and funnel plot function
+#### model1
 
-bias1=function(parDF, res2){ 
-  
- m1= metabias(
-    parDF$rr, 
-    parDF$se,
-    method.bias = "rank",
-    plotit = TRUE,
-    correct = TRUE,
-    k.min = 3
-  )
+model(meta1a)
+model(meta1a1)
+model(meta1a2)
 
- print(m1)
-  
-  
-  ############## pblication bias #############
-  
-  funnel(res2,
-        ylim = c(0.8,0)
-  )
-  
-  #trimfill(res2)
-  #metabias(res2)
-  #devtools::install_github("MathiasHarrer/dmetar")
-  
-  #library(dmetar)
-  #eggers.test(res2)
-  
-}
- 
+#### model2
 
-sens1 = function(res2){
- 
-  ################## leave one out
-  
-  met1= metainf(res2, pooled="random",)
-  forest(met1)
-  
-
-  ###### subgroup analysis ##############
-  
-  #subgp = update.meta(res2, byvar=parDF$gp)
-  #print(subgp)
-  
-}
-
-
-################### RUNNING THE FUNCTIONS ################3
-
-parDF = par1(meta2)
-res2=forest1(parDF)
-bias1(parDF, res2)
-sens1(res2)
-
-
-
-
-#################
-
-
-#method.tau = "DL"	DerSimonian-Laird estimator (DerSimonian and Laird, 1986)
-#method.tau = "PM"	Paule-Mandel estimator (Paule and Mandel, 1982)
-#method.tau = "REML"	Restricted maximum-likelihood estimator (Viechtbauer, 2005)
-#method.tau = "ML"	Maximum-likelihood estimator (Viechtbauer, 2005)
-#method.tau = "HS"	Hunter-Schmidt estimator (Hunter and Schmidt, 2015)
-#method.tau = "SJ"	Sidik-Jonkman estimator (Sidik and Jonkman, 2005)
-#method.tau = "HE"	Hedges estimator (Hedges and Olkin, 1985)
-#method.tau = "EB"	Empirical Bayes estimator (Morris, 1983)
-
-
-
-#'"RR"' for the _log risk ratio_.
-
-#'"OR"' for the _log odds ratio_.
-
-#'"RD"' for the _risk difference_.
-
-#'"AS"' for the _arcsine square root transformed risk
-#difference_ (Rücker et al., 2009).
-
-#'"PETO"' for the _log odds ratio_ estimated with Peto's
-#         method (Yusuf et al., 1985).
-
-# "MD" for mean difference
-
-
-################ methodology
-
-# ?metagen
-# metabin
-# metacont
-
+model(meta2a)
+model(meta2a1)
+model(meta2a2)
